675fe4857de6b9baf02cb94e103f63b716d589cf
VinithaKorivi <kvineetha135@gmail.com>
2025-11-14 19:22:54 +0530

    This is first java commit

---
+++ src/GITjava/Identifiers.java
@@ -0,0 +1,15 @@
+package GITjava;
+
+public class Identifiers {
+
+	public static void main(String[] args) {
+		int total=20;
+		System.out.println(total);
+		int total123=30;
+		System.out.println(total123);
+		//int 123total =40;
+		System.out.println("This is vinit");
+
+	}
+
+}
