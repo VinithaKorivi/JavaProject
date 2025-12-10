8a42c41b1949842a328c02d60328c41e38f39127
VinithaKorivi <kvineetha135@gmail.com>
2025-12-10 17:39:47 +0530

    dec 10

--- src/InterThreads/ThreadPoool.java
+++ src/InterThreads/ThreadPoool.java
@@ -1,7 +1,7 @@
 package InterThreads;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
+
 
 class Pool implements Runnable{
 	String name;
