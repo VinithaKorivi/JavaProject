a63004ef0e93e90df775d3c89030961ecf38a755
VinithaKorivi <kvineetha135@gmail.com>
2025-12-12 20:47:00 +0530

    dec 12

--- src/WrapperClasses/ToSTRINGMeth.java
+++ src/WrapperClasses/ToSTRINGMeth.java
@@ -1,10 +1,31 @@
 package WrapperClasses;
 
+import java.util.ArrayList;
+
+class STRING{
+	String name;
+	int rollno;
+	STRING(String name, int rollno){
+		this.name=name;
+		this.rollno=rollno;
+	}
+	public String toString() {
+		return "Nmae of the student "+name+" and roll no is"+rollno;
+	}
+}
 public class ToSTRINGMeth {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		STRING s=new STRING("vini", 101);
+		System.out.println(s);
+		Integer I=new Integer(10);
+		System.out.println(I);
+		ArrayList l=new ArrayList();
+		l.add("A");
+		l.add("B");
+		System.out.println(l);
+		ToSTRINGMeth m=new ToSTRINGMeth();
+		System.out.println(m);
 	}
 
 }
