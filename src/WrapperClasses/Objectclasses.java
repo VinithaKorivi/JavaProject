7097be9113d767bdfd947299eb2e961d010be6fd
VinithaKorivi <kvineetha135@gmail.com>
2025-12-13 10:30:05 +0530

    dec 12

--- src/WrapperClasses/Objectclasses.java
+++ src/WrapperClasses/Objectclasses.java
@@ -1,9 +1,19 @@
 package WrapperClasses;
 
 public class Objectclasses {
-
+  int i;
+  Objectclasses(int i){
+	  this.i=i;
+  }
+  public int hashCode() {
+	  return i;
+//	  return 100;
+  }
 	public static void main(String[] args) {
-	System.out.println("this is vinitha");
-	}
+		Objectclasses r=new Objectclasses(10);
+		Objectclasses r2=new Objectclasses(100);
+		System.out.println(r);
+		System.out.println(r2);
+		}
 
 }
