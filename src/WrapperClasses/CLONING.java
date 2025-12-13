d2adf8f2d903e1c0acde7d43b1127e8271152a39
VinithaKorivi <kvineetha135@gmail.com>
2025-12-13 15:08:18 +0530

    dec 13

--- src/WrapperClasses/CLONING.java
+++ src/WrapperClasses/CLONING.java
@@ -1,10 +1,17 @@
 package WrapperClasses;
 
-public class CLONING {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class CLONING implements Cloneable {
+    int i=10;
+    int j=20;
+	public static void main(String[] args) throws CloneNotSupportedException {
+		CLONING c=new CLONING();
+		System.out.println(c.i+"..."+c.j);
+		CLONING c1=(CLONING)c.clone();
+		c1.i=888;
+		c1.j=9999;
+		System.out.println(c.i+"..."+c.j);
+		System.out.println(c1.i+"..."+c1.j);
+		System.out.println(c.i+"..."+c.j);
 	}
 
 }
