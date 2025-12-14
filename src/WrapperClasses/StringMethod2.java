2bfed592a2ab15daa7d69eda356553d016f67ac7
VinithaKorivi <kvineetha135@gmail.com>
2025-12-14 22:39:52 +0530

    adding 2 string methods

---
+++ src/WrapperClasses/StringMethod2.java
@@ -0,0 +1,15 @@
+package WrapperClasses;
+
+public class StringMethod2 {
+
+	public static void main(String[] args) {
+		String s="Durga";
+		System.out.println(s.charAt(2));
+//		System.out.println(s.charAt(10));
+//		s=s.concat("Software");
+//		s=s+"Software";
+		s+="software";
+		System.out.println(s);
+	}
+
+}
