ce0636abb8d6f9dbcec20424b0b8b802c8548676
VinithaKorivi <kvineetha135@gmail.com>
2025-12-14 22:51:27 +0530

    adding 2 string methods

---
+++ src/WrapperClasses/StringMethod4.java
@@ -0,0 +1,12 @@
+package WrapperClasses;
+
+public class StringMethod4 {
+
+	public static void main(String[] args) {
+		String f=new String("My name is Vinitha");
+        System.out.println(f.length());
+        String s=new String("dad");
+        System.out.println(s.replace('d', 'm'));
+	}
+
+}
