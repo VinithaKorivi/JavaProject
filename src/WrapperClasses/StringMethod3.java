87903594e18debc60fd0cff59367598bf9d90daa
VinithaKorivi <kvineetha135@gmail.com>
2025-12-14 22:45:22 +0530

    adding 2 string methods

---
+++ src/WrapperClasses/StringMethod3.java
@@ -0,0 +1,14 @@
+package WrapperClasses;
+
+public class StringMethod3 {
+
+	public static void main(String[] args) {
+		String s="DURGA";
+		System.out.println(s.equals("durga"));
+       System.out.println(s.equalsIgnoreCase("durga"));
+       String t=new String("Vinitha");
+       System.out.println(t.substring(2));
+       System.out.println(t.substring(2,7));
+	}
+
+}
