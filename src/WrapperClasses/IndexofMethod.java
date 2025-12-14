f233e18dc5ece3d715458c2f435f49436b54d702
VinithaKorivi <kvineetha135@gmail.com>
2025-12-14 23:00:02 +0530

    Indexof method

---
+++ src/WrapperClasses/IndexofMethod.java
@@ -0,0 +1,13 @@
+package WrapperClasses;
+
+public class IndexofMethod {
+
+	public static void main(String[] args) {
+		String s=new String("Durga");
+		System.out.println(s.indexOf('a'));
+        System.out.println(s.lastIndexOf('D'));
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+	}
+
+}
