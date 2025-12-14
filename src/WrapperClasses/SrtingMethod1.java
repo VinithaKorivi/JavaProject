422eadbb243b3c8c50a47aa275981b555d417a8f
VinithaKorivi <kvineetha135@gmail.com>
2025-12-14 22:35:36 +0530

    adding 2 string methods

---
+++ src/WrapperClasses/SrtingMethod1.java
@@ -0,0 +1,14 @@
+package WrapperClasses;
+
+public class SrtingMethod1 {
+
+	public static void main(String[] args) {
+		char[] ch= {'a', 'b', 'c', 'd'};
+		String s=new String(ch);
+        System.out.println(s);
+        byte[] b= {100,101,102,103};
+        String t=new String(b);
+        System.out.println(t);
+	}
+
+}
