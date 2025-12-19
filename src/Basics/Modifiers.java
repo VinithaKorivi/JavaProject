bc06df4fb750a04edc3ed412e79780a436fdae2d
VinithaKorivi <kvineetha135@gmail.com>
2025-12-19 22:16:47 +0530

    modifiers

---
+++ src/Basics/Modifiers.java
@@ -0,0 +1,17 @@
+package Basics;
+class Test{
+	public void m1() {
+		System.out.println("this is the m1 method");
+	}
+}
+public class Modifiers {
+    public static void main(String[] args) {
+	  Test t=new Test();
+	  t.m1();
+	  Modifiers m=new Modifiers();
+	  m.m2();
+     }
+    private  void m2() {
+		  System.out.println("this is private m2 method");
+	  }
+}
