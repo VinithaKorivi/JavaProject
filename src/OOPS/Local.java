016d1d4334f852b2bc5f905b91c13f974832fb69
VinithaKorivi <kvineetha135@gmail.com>
2026-01-02 21:26:18 +0530

    Java OOP concept

---
+++ src/OOPS/Local.java
@@ -0,0 +1,19 @@
+package OOPS;
+class Local 
+{
+	 static int a=1420304005;
+	public static void main(String[] args) 
+	{
+		int c=2030405060;
+		System.out.println(Local.a);
+		int b=30;
+        System.out.println(b);
+		System.out.println(c);
+        byte d=20;
+		System.out.println(d);
+		short e=23040;
+		System.out.println(e);
+		 long f=401020305;
+		System.out.println(f);
+	}
+}
