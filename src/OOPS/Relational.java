023e359c9448b8b8109fec05257b04d5208193e2
VinithaKorivi <kvineetha135@gmail.com>
2026-01-02 21:28:16 +0530

    Java OOP concept

---
+++ src/OOPS/Relational.java
@@ -0,0 +1,16 @@
+package OOPS;
+class Relational 
+{
+	public static void main(String[] args) 
+	{
+		int a=90;
+		int b=10;
+		int c=20;
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a>=b);
+		System.out.println(a<=b);
+		System.out.println(a!=b);
+		System.out.println(a==b);
+	}
+}
