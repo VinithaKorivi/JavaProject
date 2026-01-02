a94c21d07ca71d0f66d74547e0b1f0e1e8fad34e
VinithaKorivi <kvineetha135@gmail.com>
2026-01-02 21:30:17 +0530

    OOP Concept

---
+++ src/OOPS/Methoddefi.java
@@ -0,0 +1,14 @@
+package OOPS;
+class Methoddefi 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi!");
+		Methoddefi.m1();
+		System.out.println("bye");
+	}
+	public static void m1()
+	{
+		System.out.println("Hello M10");
+	}
+}
