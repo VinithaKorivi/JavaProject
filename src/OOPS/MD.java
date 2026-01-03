26a10cf287b503aaaa69b9ae928648f3e8103805
VinithaKorivi <kvineetha135@gmail.com>
2026-01-03 20:33:19 +0530

    OOP Concepts

---
+++ src/OOPS/MD.java
@@ -0,0 +1,19 @@
+package OOPS;
+class MD 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi");
+		MD.m1();
+		System.out.println("what are you doing madam");
+	}
+	public static void m1() 
+	{
+		
+		int a=20;
+		int b=30;
+		int c=a+b;
+		System.out.println("add of a and b is"+c);
+	}
+}
+
