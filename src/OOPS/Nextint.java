14ab16780318b1ba7479925d76c9ea2c5661ba74
VinithaKorivi <kvineetha135@gmail.com>
2026-01-03 20:34:33 +0530

    OOP Concepts

---
+++ src/OOPS/Nextint.java
@@ -0,0 +1,12 @@
+package OOPS;
+import java.util.Scanner;
+class Nextint 
+{
+	public static void main(String[] args) 
+	{
+		Scanner scn=new Scanner(System.in);
+		System.out.println("enter a name");
+		String  name=scn.nextLine();
+		System.out.println("entered name is="+name);
+	}
+}
