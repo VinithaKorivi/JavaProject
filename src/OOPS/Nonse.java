da429fa3a45e60e590ccf03323c1fa533030fbad
VinithaKorivi <kvineetha135@gmail.com>
2026-01-03 20:33:25 +0530

    OOP Concepts

---
+++ src/OOPS/Nonse.java
@@ -0,0 +1,18 @@
+package OOPS;
+class Nonse
+{
+	int x=20;
+	int y=40;
+	public static void main(String[] args) 
+	{
+		Nonse g2=new Nonse();
+		System.out.println(g2.x);
+		System.out.println(g2.y);
+		Nonse g1=new Nonse();
+		System.out.println(g1.x);
+		System.out.println(g1.y);
+		Nonse g3=new Nonse();
+		System.out.println(g3.x);
+		System.out.println(g2.y);
+	}
+}
