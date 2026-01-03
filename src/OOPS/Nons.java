e828de8d0f5e2bf23fc7d572a6092b3bf801ab87
VinithaKorivi <kvineetha135@gmail.com>
2026-01-03 20:34:04 +0530

    OOP Concepts

---
+++ src/OOPS/Nons.java
@@ -0,0 +1,13 @@
+package OOPS;
+class Nons 
+{
+	int a=20;
+	static int c=50;
+	public static void main(String[] args) 
+	{ 
+		Nons scn=new Nons();
+		System.out.println(scn.a);
+		int c=20;
+		System.out.println(c);
+	}
+}
