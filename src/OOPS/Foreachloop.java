892aeed5ecbe6a6451ef94816f280a4082b88357
VinithaKorivi <kvineetha135@gmail.com>
2026-01-04 20:37:43 +0530

    OOPS Concept

---
+++ src/OOPS/Foreachloop.java
@@ -0,0 +1,12 @@
+package OOPS;
+class Foreachloop 
+{
+	public static void main(String[] args) 
+	{
+		int[] a={1,2,3,4,5,6,7,8,9};
+		for(int e:a)
+		{
+			System.out.println("values of e is"+e);
+		}
+	}
+}
