eeccfa88646d75d1218224d4d6ce50320b48724b
VinithaKorivi <kvineetha135@gmail.com>
2026-01-04 20:38:16 +0530

    OOPS Concept

---
+++ src/OOPS/Scanc.java
@@ -0,0 +1,12 @@
+package OOPS;
+import java.util.Scanner;
+class Scanc 
+{
+	public static void main(String[] args) 
+	{
+		Scanner scn=new Scanner(System.in);
+		System.out.println("enter a number!");
+		int num=scn.nextInt();
+        System.out.println("entered number is!"+ num);
+	}
+}
