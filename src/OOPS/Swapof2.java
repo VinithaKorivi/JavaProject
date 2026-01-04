ef7946566a89a142b9d57f156bef367372d87afd
VinithaKorivi <kvineetha135@gmail.com>
2026-01-04 20:39:07 +0530

    Recursion programs

---
+++ src/OOPS/Swapof2.java
@@ -0,0 +1,19 @@
+package OOPS;
+class Swapof2 
+{
+	public static void main(String[] args) 
+	{
+		int a=30;
+		int b=20;
+		int c=0;
+		System.out.println("a value is"+a);
+		System.out.println("b value is"+b);
+		System.out.println("c value is"+c);
+        c=a;
+		a=b;
+		b=c;
+		System.out.println("a value is"+a);
+		System.out.println("b value is"+b);
+		System.out.println("c value is"+c);
+	}
+}
