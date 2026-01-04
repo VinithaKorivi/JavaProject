1a741658d1d1bd7ef4950ef134ac06b919c33c1a
VinithaKorivi <kvineetha135@gmail.com>
2026-01-04 20:37:38 +0530

    OOPS Concept

---
+++ src/OOPS/InnerClass.java
@@ -0,0 +1,34 @@
+package OOPS;
+
+public class InnerClass {
+
+	public static void main(String[] args) 
+	{
+		int a=90;
+		int b=10;
+		int c=20;
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a>=b);
+		System.out.println(a<=b);
+		System.out.println(a!=b);
+		System.out.println(a==b);
+
+		System.out.println("CLASS MAIN STARTS");
+		System.out.println("CLASS  STARTS");
+		System.out.println(" MAIN STARTS");
+		System.out.println("CLASS MAIN ");
+		System.out.println(" STARTS");
+	}
+}
+class R
+{
+	public static void main(String[] args)
+	{
+		System.out.println("what baby");
+		System.out.println("what are you doing");
+		System.out.println(" iam studying");
+		System.out.println("what about you");
+	}
+
+}
