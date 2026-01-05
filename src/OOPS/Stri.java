e522363eda937320fbaeabbebf5118df9260d6d1
VinithaKorivi <kvineetha135@gmail.com>
2026-01-05 20:52:34 +0530

    OOPs concept

---
+++ src/OOPS/Stri.java
@@ -0,0 +1,16 @@
+package OOPS;
+class Stri 
+{
+	public static void main(String[] args) 
+	{
+		String name="vineetha";
+		System.out.println(name);
+		boolean hasgirlfriend=true;
+		boolean nogirlfriend=false;
+		boolean hasboyandgirl=false;
+		boolean ihave=false;
+		System.out.println(nogirlfriend);
+		System.out.println(hasboyandgirl);
+		System.out.println(hasgirlfriend);
+	}
+}
