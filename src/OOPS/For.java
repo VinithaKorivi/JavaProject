cbbe55b25c687c61a3126309e2f07b787f2aa873
VinithaKorivi <kvineetha135@gmail.com>
2026-01-05 20:52:52 +0530

    OOPS Concept

---
+++ src/OOPS/For.java
@@ -0,0 +1,14 @@
+package OOPS;
+
+public class For {
+
+		public static void main(String[] args) 
+		{
+			int[] a={3,5,6,7,7,8};
+	        for (int n:a)
+	        {
+	          System.out.println(n);
+	        }
+		}
+	
+}
