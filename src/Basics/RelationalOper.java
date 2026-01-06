b63c7569ffcca1c5cec736a16b4b19816fa2de1a
VinithaKorivi <kvineetha135@gmail.com>
2026-01-06 22:34:49 +0530

    Relational operators

---
+++ src/Basics/RelationalOper.java
@@ -0,0 +1,16 @@
+package Basics;
+
+public class RelationalOper {
+
+	public static void main(String[] args) {
+	char c='B';
+	System.out.println(c>'A');
+	System.out.println('a'>97.6);
+	double x = 10.5, y = 10.4;
+	System.out.println(x >= y); 
+	char c1 = 'A', c2 = 'B';
+	System.out.println(c1 < c2);
+	
+	}
+
+}
