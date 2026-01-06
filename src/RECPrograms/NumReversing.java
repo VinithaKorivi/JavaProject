96982cbed5a5aaac6c0649684cc04350594995cf
VinithaKorivi <kvineetha135@gmail.com>
2026-01-06 22:34:12 +0530

    Num reversing

---
+++ src/RECPrograms/NumReversing.java
@@ -0,0 +1,16 @@
+package RECPrograms;
+
+public class NumReversing {
+
+	public static void main(String[] args) {
+		int num=1234567891;
+		int rev=0;
+		while(num!=0) {
+			 rev=rev*10+num%10;
+			 num=num/10;
+		}
+		System.out.println(rev);
+
+	}
+
+}
