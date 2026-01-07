802f5f9457ebc7bb220c7a6f0810155849b67e37
VinithaKorivi <kvineetha135@gmail.com>
2026-01-07 21:24:11 +0530

    Increment operators

---
+++ src/PracticePrograms/IncrementOper.java
@@ -0,0 +1,18 @@
+package PracticePrograms;
+
+public class IncrementOper {
+
+	public static void main(String[] args) {
+		int a=10;
+		System.out.println(a++);
+		System.out.println(a);
+        System.out.println(++a + "  "+--a);
+        System.out.println("=============");
+        int x=23;
+        System.out.println(--x);
+        System.out.println(x);
+        System.out.println(x--);
+        System.out.println(x);
+	}
+
+}
