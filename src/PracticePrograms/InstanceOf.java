65a5c377336fcebd5c3203eb78286376c2ab8683
VinithaKorivi <kvineetha135@gmail.com>
2026-01-07 21:24:26 +0530

    Instance of operator

---
+++ src/PracticePrograms/InstanceOf.java
@@ -0,0 +1,19 @@
+package PracticePrograms;
+
+public class InstanceOf {
+
+	public static void main(String[] args) {
+		int[] a= {1,2,3};
+		System.out.println(a instanceof int[]);
+        System.out.println(a instanceof Object);
+        Object obj="java";
+        if(obj instanceof String s) {
+        	System.out.println(s.length());
+        	Thread t=new Thread();
+        	System.out.println(t instanceof Thread);
+        	 System.out.println(t instanceof Runnable);
+        	 System.out.println(t instanceof Object);
+        }
+	}
+
+}
