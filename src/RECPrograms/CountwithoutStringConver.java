f9e72df304512cb47b7aca11a9cdb5235d0582f0
VinithaKorivi <kvineetha135@gmail.com>
2026-01-08 18:48:15 +0530

    Count with out string

---
+++ src/RECPrograms/CountwithoutStringConver.java
@@ -0,0 +1,15 @@
+package RECPrograms;
+
+public class CountwithoutStringConver {
+
+	public static void main(String[] args) {
+		int num=123654586;
+		int count=0;
+		while(num>0) {
+			count++;
+			num/=10;
+		}
+   System.out.println("the no of digits in input is  "+count);
+	}
+
+}
