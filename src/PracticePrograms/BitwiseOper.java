7e24b91dfd72d3ecfec80cdb955e5d66839cb02c
VinithaKorivi <kvineetha135@gmail.com>
2026-01-08 18:47:23 +0530

    Bitwise Operator

---
+++ src/PracticePrograms/BitwiseOper.java
@@ -0,0 +1,20 @@
+package PracticePrograms;
+
+public class BitwiseOper {
+
+	public static void main(String[] args) {
+		int a=12;
+		int b=10;
+		System.out.println(a&b);
+		int a1=5;
+		int b1=3;
+		System.out.println(a1|b1);
+		int x=6;
+		int y=3;
+		System.out.println(x^y);
+		 int g = 5;
+	        System.out.println(~g);
+
+	}
+
+}
