8add3b3dd26f2c416bb70afa2b91cf1885289e9e
VinithaKorivi <kvineetha135@gmail.com>
2026-01-09 21:28:24 +0530

    Private method

---
+++ src/PracticePrograms/Private.java
@@ -0,0 +1,13 @@
+package PracticePrograms;
+class Pri{
+	//private static int y=20;
+	
+}
+public class Private {
+  private static  int x=10;
+	public static void main(String[] args) {
+		System.out.println(x);
+//       System.out.println(y);
+	}
+
+}
