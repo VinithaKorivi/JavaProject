0f278348c23b7fae69803ea17bb914b1358f284f
VinithaKorivi <kvineetha135@gmail.com>
2026-01-09 21:28:50 +0530

    short circuit operator

---
+++ src/PracticePrograms/ShortCircuitOper.java
@@ -0,0 +1,17 @@
+package PracticePrograms;
+
+public class ShortCircuitOper {
+
+	public static void main(String[] args) {
+		int x=10;
+		int y=5;
+		System.out.println(x&y);
+		System.out.println(x|y);
+		if(++x<10 && ++y>15) {
+			x++;
+		}
+		else { y++; }
+		System.out.println(x+"   "+y);
+	}
+
+}
