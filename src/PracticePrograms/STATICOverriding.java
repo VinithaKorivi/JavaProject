0ec32b7b7fcc9da1f1cdcd33285e6f2a932b720e
VinithaKorivi <kvineetha135@gmail.com>
2026-01-09 21:29:17 +0530

    Overriding static method

---
+++ src/PracticePrograms/STATICOverriding.java
@@ -0,0 +1,15 @@
+package PracticePrograms;
+
+public class STATICOverriding {
+
+	public static void main(String[] args) {
+      System.out.println("This is main method");
+      
+	}
+
+}
+class Over{
+	public static void main(String[] args) {
+		System.out.println("This is the overriden method of main method");
+	}
+} 
