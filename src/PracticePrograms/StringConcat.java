4ad26d4cd8a76e086178d366c8e82229e1802880
VinithaKorivi <kvineetha135@gmail.com>
2026-01-10 23:43:59 +0530

    String concatination example

---
+++ src/PracticePrograms/StringConcat.java
@@ -0,0 +1,24 @@
+package PracticePrograms;
+
+public class StringConcat {
+     public static String getdate() {
+    	 return "java"; 
+    	 }
+	public static void main(String[] args) {
+		String s='A'+10+"java";
+      System.out.println(s);
+      String d="java"+"World";
+      String s2="java";
+      String s3=s2+"world";
+      System.out.println(d==s3);
+      System.out.println("============");
+      
+      String a='A'+'B'+getdate()+"developer";
+      System.out.println(a);
+      
+      System.out.println("=========");
+      String s4 = 10 + 20 + "Java" + 30 + 40;
+      System.out.println(s4);
+	}
+
+}
