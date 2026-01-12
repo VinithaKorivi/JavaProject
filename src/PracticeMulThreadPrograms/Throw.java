7570ea6a569d93b1ce16c1a842fe1cae119d0225
VinithaKorivi <kvineetha135@gmail.com>
2026-01-12 23:20:55 +0530

    Throw keyword

---
+++ src/PracticeMulThreadPrograms/Throw.java
@@ -0,0 +1,18 @@
+package PracticeMulThreadPrograms;
+
+public class Throw {
+	static ArithmeticException e=new ArithmeticException();
+	static ArithmeticException n;
+	public static void main(String[] args) {
+		throw n;
+//		throw e;
+		
+		
+//		System.out.println("The following exception object is created by vinitha");
+//	     throw new ArithmeticException("by zero");   
+//		 System.out.println("Exception object will be created by JVM");    
+//		 System.out.println(10/0);
+//		  
+	}
+
+}
