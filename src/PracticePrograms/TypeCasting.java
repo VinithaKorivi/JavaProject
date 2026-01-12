a8d61ddf4c981c257b358bf1dc2e44c956c2952f
VinithaKorivi <kvineetha135@gmail.com>
2026-01-12 23:22:46 +0530

    Typecasting example program

---
+++ src/PracticePrograms/TypeCasting.java
@@ -0,0 +1,16 @@
+package PracticePrograms;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		int x=123;
+		byte b=(byte)x;
+		System.out.println(b);
+		int y='z';
+		System.out.println(y);
+       int x1=34567;
+       byte b1=(byte)x1;
+       System.out.println(b1);
+	}
+
+}
