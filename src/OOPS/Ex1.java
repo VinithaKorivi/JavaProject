7c4da8b753e3b6feab6c2b78a47a2858bc48d514
VinithaKorivi <kvineetha135@gmail.com>
2026-01-13 21:03:42 +0530

    OOps basic concept

---
+++ src/OOPS/Ex1.java
@@ -0,0 +1,21 @@
+package OOPS;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+
+        System.out.println("Array Elements:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Index " + i + " = " + arr[i]);
+        }
+
+        System.out.println("Length = " + arr.length);
+
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
