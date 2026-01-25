54b410fa572e411eac4ce13e1123506252e32206
VinithaKorivi <kvineetha135@gmail.com>
2026-01-25 21:08:05 +0530

    Scanner class

---
+++ src/Basics/ScannerClass.java
@@ -0,0 +1,32 @@
+package Basics;
+import java.util.Scanner;
+public class ScannerClass {
+
+	public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+//    System.out.println("Enter your number");
+//    int a=sc.nextInt();
+//    System.out.println("your entered no is "+a);
+//    System.out.println("*************");
+//    System.out.println("Enter your float number");
+//    float b=sc.nextFloat();
+//    System.out.println("your entered float no is "+b);
+//    System.out.println("*************");
+//    System.out.println("Enter your long number");
+//    long c=sc.nextLong();
+//    System.out.println("your entered long no is "+c);
+//    System.out.println("*************");
+//    System.out.println("Enter your double number");
+//    double d=sc.nextDouble();
+//    System.out.println("your entered double no is "+d);
+//    System.out.println("*************");
+//    System.out.println("Enter your word");
+//    String word=sc.next();
+//    System.out.println("your entered word  is "+word);
+    System.out.println("*************");
+    System.out.println("Enter your sentence");
+    String fullname=sc.nextLine();
+    System.out.println("your entered  sentence is "+fullname);
+	}
+
+}
