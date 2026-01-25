44b2ee9ec5d07dcfa134a4b74e40927bab0a3783
VinithaKorivi <kvineetha135@gmail.com>
2026-01-25 21:07:32 +0530

    Constructor Overloading

---
+++ src/OOPS/ConstructorOverloading.java
@@ -0,0 +1,15 @@
+package OOPS;
+
+public class ConstructorOverloading {
+	public ConstructorOverloading(){
+//		this(10);
+	}
+	public ConstructorOverloading(int i){
+//		this();
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+
+}
