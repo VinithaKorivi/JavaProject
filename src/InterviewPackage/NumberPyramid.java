31bfe687ed79bdca6848e588b0807941a5bae7d4
VinithaKorivi <kvineetha135@gmail.com>
2026-02-11 22:10:42 +0530

    Number pyramid

---
+++ src/InterviewPackage/NumberPyramid.java
@@ -0,0 +1,18 @@
+package InterviewPackage;
+
+public class NumberPyramid {
+    public static void main(String[] args) {
+        int n = 5;
+        int num = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++)
+                System.out.print(" ");
+
+            for (int j = 1; j <= i; j++)
+                System.out.print(num++ + " ");
+
+            System.out.println();
+        }
+    }
+}
