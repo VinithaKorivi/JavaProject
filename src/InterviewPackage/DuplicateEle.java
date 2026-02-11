73458cd31a9e50b8bcc1ff160dd01ee64155b6be
VinithaKorivi <kvineetha135@gmail.com>
2026-02-11 22:10:21 +0530

    Duplicate elem

---
+++ src/InterviewPackage/DuplicateEle.java
@@ -0,0 +1,35 @@
+package InterviewPackage;
+import java.util.Scanner;
+
+public class DuplicateEle {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter array size:");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Duplicate elements are:");
+
+        for (int i = 0; i < size; i++) {
+
+            for (int j = i + 1; j < size; j++) {
+
+                if (arr[i] == arr[j]) {
+                    System.out.println(arr[i]);
+                    break;
+                }
+            }
+        }
+
+        sc.close();
+    }
+}
