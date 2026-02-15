3eb6210ea5f34ef666dcc71e67f0afa6570044fd
VinithaKorivi <kvineetha135@gmail.com>
2026-02-15 21:35:23 +0530

    Pattern 11

---
+++ src/PatternPrograms/Pattern11.java
@@ -0,0 +1,20 @@
+package PatternPrograms;
+
+import java.util.Scanner;
+
+public class Pattern11 {
+
+	public static void main(String[] args) {
+	
+		Scanner scn=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n=scn.nextInt();
+		for(int i=1;i<=5;i++) {
+	    	for(int j=1;j<=3;j++) {
+	    		System.out.print(i+" "+j+" ");
+	    	}
+	    	System.out.println();
+	
+	}}
+
+}
