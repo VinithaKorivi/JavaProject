2b1485a9bdc75de10e7507ed30c4086c15254e87
VinithaKorivi <kvineetha135@gmail.com>
2026-02-23 20:41:16 +0530

    Pattern

---
+++ src/PatternPrograms/Pattern13.java
@@ -0,0 +1,25 @@
+package PatternPrograms;
+
+public class Pattern13 {
+
+	public static void main(String[] args) {
+   int n=5;
+   for(int i=1;i<=n;i++) {
+	   int x=i;
+	   int y=n-i+1;
+	   for(int j=1;j<=n;j++) {
+		   if(j%2==1)
+		   {
+			   System.out.printf("%2d ",x);
+		   }
+		   else {
+			   System.out.printf("%2d ",y);
+		   }
+		   x=x+n;
+		   y=y+n;
+	   }
+	   System.out.println();
+   }
+	}
+
+}
