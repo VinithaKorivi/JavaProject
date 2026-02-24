1785f189a872c78c9cb51c528d211de1a8933692
VinithaKorivi <kvineetha135@gmail.com>
2026-02-24 21:39:45 +0530

    basics

---
+++ src/Basics/BitwisecomplementOper.java
@@ -0,0 +1,18 @@
+package Basics;
+
+public class BitwisecomplementOper {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 10;
+        int c = 0;
+        int d = -1;
+        int e = 7;
+
+        System.out.println("~5  = " + (~a));
+        System.out.println("~10 = " + (~b));
+        System.out.println("~0  = " + (~c));
+        System.out.println("~-1 = " + (~d));
+        System.out.println("~7  = " + (~e));
+    }
+}
