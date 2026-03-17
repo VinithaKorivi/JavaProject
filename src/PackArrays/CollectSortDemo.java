81f0578270f9e56e6ceb00f547706fd7d09647d0
VinithaKorivi <kvineetha135@gmail.com>
2026-03-17 21:07:51 +0530

    Sorting

---
+++ src/PackArrays/CollectSortDemo.java
@@ -0,0 +1,18 @@
+package PackArrays;
+import java.util.*;
+public class CollectSortDemo {
+
+	public static void main(String[] args) {
+//      ArrayList l=new ArrayList();
+//      l.add("10");
+//      l.add("20");
+//      l.add("30");
+//      l.add("40");
+//      l.add("50");
+//      l.add("60");
+      
+      System.out.println(System.getProperty("java.version"));
+      
+	}
+
+}
