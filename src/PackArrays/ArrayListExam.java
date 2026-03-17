3a0f86596b6aa8403b2802d3c092ddb82557e933
VinithaKorivi <kvineetha135@gmail.com>
2026-03-17 21:07:37 +0530

    Arraylist example

---
+++ src/PackArrays/ArrayListExam.java
@@ -0,0 +1,18 @@
+package PackArrays;
+
+import java.util.*;
+
+import java.util.*;
+
+public class ArrayListExam {
+    public static void main(String[] args) {
+
+        SortCustom list = new SortCustom();
+
+//        list.add("Java");
+//        list.add("Python");
+//        list.add("C++");
+
+        System.out.println(list);
+    }
+}
