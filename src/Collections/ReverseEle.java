98652e5b545d3f6152073e7b134f23818d1a249e
VinithaKorivi <kvineetha135@gmail.com>
2026-03-26 20:56:22 +0530

    reversing an element

---
+++ src/Collections/ReverseEle.java
@@ -0,0 +1,18 @@
+package Collections;
+import java.util.*;
+public class ReverseEle {
+
+	public static void main(String[] args) {
+    ArrayList l=new ArrayList();
+    l.add(1);
+    l.add(2);
+    l.add(3);
+    l.add(4);
+    l.add(5);
+    l.add(6);
+    System.out.println(l);
+    Collections.reverse(l);
+    System.out.println(l);
+	}
+
+}
