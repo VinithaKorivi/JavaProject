d7186754edace8af90313761c45951dab8f6eb4f
VinithaKorivi <kvineetha135@gmail.com>
2026-03-28 20:19:52 +0530

    concurrent topic

---
+++ src/ACollections/Concurrent.java
@@ -0,0 +1,29 @@
+package ACollections;
+import java.util.*;
+public class Concurrent extends Thread {
+	static ArrayList l=new ArrayList();
+    public void run() {
+    	try {
+    		Thread.sleep(2000);
+    	}catch(InterruptedException e) {  }
+    		System.out.println("child thread updating list");
+    		l.add("a");
+    	
+    }
+	public static void main(String[] args) throws InterruptedException {
+		l.add("h");
+		l.add("g");
+		l.add("p");
+		l.add("z");
+		Concurrent c=new Concurrent();
+		c.start();
+		Iterator itr=l.iterator();
+		while(itr.hasNext()) {
+			String s1=(String)itr.next();
+			System.out.println("main thread iterating list and current object is "+s1);
+			Thread.sleep(3000);
+		}
+     System.out.println(l);
+	}
+
+}
