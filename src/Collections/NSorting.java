1138542ce8c1f05846999a71d892e2fca481df5a
VinithaKorivi <kvineetha135@gmail.com>
2026-03-28 20:20:26 +0530

    natural sorting

---
+++ src/Collections/NSorting.java
@@ -0,0 +1,17 @@
+package Collections;
+import java.util.*;
+public class NSorting {
+
+	public static void main(String[] args) {
+     ArrayList l=new ArrayList();
+     l.add(10);
+     l.add(80);
+     l.add(30);
+     l.add(40);
+     l.add(50);
+     System.out.println(l);
+     Collections.sort(l);
+     System.out.println(l);
+	}
+
+}
