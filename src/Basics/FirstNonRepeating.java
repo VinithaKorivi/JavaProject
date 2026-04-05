fe7070da3588ab7a4c1478c7fc44d0e2ceb483bd
VinithaKorivi <kvineetha135@gmail.com>
2026-04-05 22:19:49 +0530

    first non repeating

---
+++ src/Basics/FirstNonRepeating.java
@@ -0,0 +1,23 @@
+package Basics;
+
+public class FirstNonRepeating {
+
+	public static void main(String[] args) {
+		        String str = "swiss";
+
+		        for(int i = 0; i < str.length(); i++) {
+		            boolean unique = true;
+		            for(int j = 0; j < str.length(); j++) {
+		                if(i != j && str.charAt(i) == str.charAt(j)) {
+		                    unique = false;
+		                    break;
+		                }
+		            }
+		            if(unique) {
+		                System.out.println(str.charAt(i));
+		                break;
+		            }
+		        }
+	}
+
+}
