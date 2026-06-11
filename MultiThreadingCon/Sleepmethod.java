3964515552d966e9ac2b0ee5acc3996347786dc5
VinithaKorivi <kvineetha135@gmail.com>
2026-06-11 22:02:10 +0530

    sleep method

---
+++ MultiThreadingCon/Sleepmethod.java
@@ -0,0 +1,26 @@
+package MultiThreadingCon;
+class Demo6 extends Thread{
+	public void run() {
+		for(int i=0;i<=3;i++) {
+			System.out.print("Child Thread ");
+			try{
+				Thread.sleep(2000);
+			}catch(InterruptedException e) {
+				System.out.print("I got interrupted");
+			}
+			System.out.println(" is running");
+		}
+		
+	}
+}
+public class Sleepmethod {
+
+	public static void main(String[] args) {
+		Demo6 d=new Demo6();
+		d.start();
+		for(int i=0;i<=3;i++) {
+			System.out.println("Main Thread ");
+		}
+	}
+
+}
