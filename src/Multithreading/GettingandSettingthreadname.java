9bd039fd15786a60727ac4114cdb0702a771264d
VinithaKorivi <kvineetha135@gmail.com>
2026-06-12 22:28:55 +0530

    get and set name

---
+++ src/Multithreading/GettingandSettingthreadname.java
@@ -0,0 +1,20 @@
+package Multithreading;
+class Demo9 extends Thread{
+	public void run() {
+		for(int i=0;i<=2;i++) {
+			System.out.println("This is about getting and setting name of a thread");
+		}
+	}
+}
+public class GettingandSettingthreadname {
+
+	public static void main(String[] args) {
+    System.out.println(Thread.currentThread().getName());
+    Demo9 t=new Demo9();
+    t.start();
+    System.out.println(t.getName());
+    Thread.currentThread().setName("Vinitha");
+    System.out.println("current Thread name is "+Thread.currentThread().getName());
+	}
+
+}
