31d931eb447a1d7469190eb1f84db5fff6df04bb
VinithaKorivi <kvineetha135@gmail.com>
2026-06-16 22:38:30 +0530

    command

--- src/OOPS/Command.java
+++ src/OOPS/Command.java
@@ -9,6 +9,8 @@ public class Command {
 
 	public static void main(String[] args) {
 		System.out.println("Hi");
+		Command c=new Command();
+		c.Vinitha();
 		for(int i=0;i<args.length;i++) {
 			System.out.println(args[i]);
 		}
