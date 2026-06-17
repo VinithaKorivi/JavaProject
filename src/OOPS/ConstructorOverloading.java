ce20eb063c86f5b3d91fc2394c7ac7fceb925266
VinithaKorivi <kvineetha135@gmail.com>
2026-06-17 21:45:59 +0530

    const overload

--- src/OOPS/ConstructorOverloading.java
+++ src/OOPS/ConstructorOverloading.java
@@ -2,14 +2,19 @@ package OOPS;
 
 public class ConstructorOverloading {
 	public ConstructorOverloading(){
-//		this(10);
+	this(10);
+		System.out.println("all these executed by  no arg constr");
 	}
 	public ConstructorOverloading(int i){
+		
 //		this();
+		System.out.println("this is constructor");
+		System.out.println(i);
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		ConstructorOverloading v=new ConstructorOverloading();
 	}
 
 }
