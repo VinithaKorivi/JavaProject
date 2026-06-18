d5bfe31e42ffbe00b0ea0c4634d65f257eb0d049
VinithaKorivi <kvineetha135@gmail.com>
2026-06-18 23:23:39 +0530

    jdbc demo

--- src/ZJDBC/Demojdbc.java
+++ src/ZJDBC/Demojdbc.java
@@ -5,11 +5,11 @@ public class Demojdbc {
 	public static void main(String[] args) throws Exception{
 		String url = "jdbc:mysql://localhost:3306/employee";
 		String username = "root";
-		 String password = "Tiger@1";
-		 Connection con = DriverManager.getConnection(url, username, password);
-		 Statement st = con.createStatement();
-		 ResultSet rs = st.executeQuery("SELECT * FROM emp");
-		 while(rs.next()) {
+		String password = "Tiger@1";
+		Connection con = DriverManager.getConnection(url, username, password);
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery("SELECT * FROM emp");
+		while(rs.next()) { 
 	            System.out.println(
 	                rs.getInt("id") + " " +
 	                rs.getString("name") + " " +
